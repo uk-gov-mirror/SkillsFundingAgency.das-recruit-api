6e0a8417e87ba3c7490d800040d2f21ac64e4925
killij <51908793+killij@users.noreply.github.com>
2026-08-03 13:33:06 +0100

    FAI-3467 Upgrade packages

--- src/Recruit.Api.IntegrationTests/GlobalUsings.cs
+++ src/Recruit.Api.IntegrationTests/GlobalUsings.cs
@@ -1,7 +1,7 @@
 ﻿global using NUnit.Framework;
 global using FluentAssertions;
 global using Moq;
-global using AutoFixture.NUnit3;
+global using AutoFixture.NUnit4;
 global using SFA.DAS.Testing.AutoFixture;
 global using AutoFixture;
 global using SFA.DAS.Recruit.Api.UnitTests.Data.DatabaseMock;
--- src/Recruit.Api.UnitTests/GlobalUsings.cs
+++ src/Recruit.Api.UnitTests/GlobalUsings.cs
@@ -1,6 +1,6 @@
 ﻿global using NUnit.Framework;
 global using FluentAssertions;
 global using Moq;
-global using AutoFixture.NUnit3;
+global using AutoFixture.NUnit4;
 global using SFA.DAS.Testing.AutoFixture;
 global using AutoFixture;
